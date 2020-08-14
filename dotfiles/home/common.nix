83b5b38c7927fe1fab700ba1802cdc9a316877ec
Paho Lurie-Gregg <paho@paholg.com>
2020-08-14 13:50:01 -0700

    home-manager: nest programs

--- dotfiles/home/common.nix
+++ dotfiles/home/common.nix
@@ -5,28 +5,30 @@
 
   home.packages = with pkgs; [ diffr exa ];
 
-  programs.home-manager = {
-    enable = true;
-    path = "...";
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    # enableVteIntegration = true;
-    defaultKeymap = "emacs";
-    history = {
-      expireDuplicatesFirst = true;
-      ignoreDups = true;
-      save = 100000;
-      size = 100000;
-      share = true;
-    };
-    zplug = {
+  programs = {
+    home-manager = {
       enable = true;
-      plugins = [{ name = "agkozak/zsh-z"; }];
+      path = "...";
     };
-    initExtra = (builtins.readFile ./zsh_extra.sh);
+
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      # enableVteIntegration = true;
+      defaultKeymap = "emacs";
+      history = {
+        expireDuplicatesFirst = true;
+        ignoreDups = true;
+        save = 100000;
+        size = 100000;
+        share = true;
+      };
+      zplug = {
+        enable = true;
+        plugins = [{ name = "agkozak/zsh-z"; }];
+      };
+      initExtra = (builtins.readFile ./zsh_extra.sh);
+    };
   };
 }
