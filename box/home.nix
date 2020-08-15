a405eed87cf3052ea6a63424cf1dd88fc77a9c86
Paho Lurie-Gregg <paho@paholg.com>
2020-08-15 02:43:57 -0700

    home-manager: Add config for box

---
+++ box/home.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ../home/common.nix ../home/gui.nix ];
+
+  home = {
+    username = "paho";
+    homeDirectory = "/home/paho";
+  };
+
+  programs.git.userEmail = "paho@paholg.com";
+
+  programs.zsh.sessionVariables = {
+    ZSH_USER_COLOR = "blue";
+    ZSH_HOST_COLOR = "magenta";
+  };
+}
