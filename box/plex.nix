ad6e47b8b2899d81d14a67e3effc6258a219854c
Paho Lurie-Gregg <paho@paholg.com>
2020-08-16 00:33:29 -0700

    box: fix plex

--- box/plex.nix
+++ box/plex.nix
@@ -8,13 +8,13 @@
     package = pkgs.plex.overrideAttrs (x:
       let
         version = "1.20.0.3181-0800642ec";
-        sha256 = "4d090a3f95193052382325a54dc3356cef7e253b";
+        sha1 = "4d090a3f95193052382325a54dc3356cef7e253b";
       in {
         name = "plex-${version}";
         src = pkgs.fetchurl {
           url =
             "https://downloads.plex.tv/plex-media-server-new/${version}/redhat/plexmediaserver-${version}.x86_64.rpm";
-          inherit sha256;
+          inherit sha1;
         };
       });
   };
