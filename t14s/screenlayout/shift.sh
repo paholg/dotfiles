770019df2f173436184f14f0c982d4c9a262083e
Paho Lurie-Gregg <paho@paholg.com>
2021-01-31 23:00:51 -0800

    t14s: add screenlayouts

---
+++ t14s/screenlayout/shift.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output HDMI-A-0 --mode 2560x1440 --pos 4080x0 --rotate normal \
+       --output DisplayPort-0 --mode 3840x2160 --pos 1920x0 --rotate left \
+       --output DisplayPort-1 --off
