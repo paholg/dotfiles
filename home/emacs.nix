9283ac68ebebcb9c68608029ca0cd2a5cfc8e859
Paho Lurie-Gregg <paho@paholg.com>
2021-02-05 10:07:00 -0800

    emacs: fix issue with lsp--on-idle

--- home/emacs.nix
+++ home/emacs.nix
@@ -145,6 +145,7 @@ in {
                 lsp-eldoc-render-all nil
                 lsp-idle-delay 0
                 lsp-enable-snippet nil
+                lsp-enable-links nil
                 company-minimum-prefix-length 1
                 company-idle-delay 0
                 gc-cons-threshold 104857600 ;; 100 MB
