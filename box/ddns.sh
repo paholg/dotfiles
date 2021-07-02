c17e4ad8fbffdce5b3c3a62074c48ab142f12bce
Paho Lurie-Gregg <paho@paholg.com>
2021-07-01 21:47:20 -0700

    box: update ddns script

--- box/ddns.sh
+++ box/ddns.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # This script gets the external IP of your systems then connects to the Gandi
 # LiveDNS API and updates your dns record with the IP.
 
-API_KEY=`cat API_KEY`
+API_KEY=$(cat API_KEY)
 
 SUBDOMAIN="home"
 DOMAIN="paholg.com"
 
 # Get external IP address
 EXT_IP=$(curl -s ifconfig.me)
+CUR_IP=$(dig +short home.paholg.com)
+
+
+if [ "$EXT_IP" = "$CUR_IP" ]; then
+    echo "IP $CUR_IP current, exiting."
+    exit 0
+fi
+
+echo "home.paholg.com is $CUR_IP, should be $EXT_IP. Updating."
 
 #Get the current Zone for the provided domain
 CURRENT_ZONE_HREF=$(curl -s -H "X-Api-Key: $API_KEY" https://dns.api.gandi.net/api/v5/domains/$DOMAIN | jq -r '.zone_records_href')
