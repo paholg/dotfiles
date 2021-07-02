1bb6a0c63cd7a502c0facd4bc1880af39b9c0bb6
Paho Lurie-Gregg <paho@paholg.com>
2021-07-01 21:37:39 -0700

    box: update gandi ddns script

--- box/ddns.sh
+++ box/ddns.sh
@@ -1,4 +1,24 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p python2 python27Packages.requests
+#!/usr/bin/env bash
 
-python /home/paho/git/gandi-live-dns/src/gandi-live-dns.py
+# This script gets the external IP of your systems then connects to the Gandi
+# LiveDNS API and updates your dns record with the IP.
+
+API_KEY=`cat API_KEY`
+
+SUBDOMAIN="home"
+DOMAIN="paholg.com"
+
+# Get external IP address
+EXT_IP=$(curl -s ifconfig.me)
+
+#Get the current Zone for the provided domain
+CURRENT_ZONE_HREF=$(curl -s -H "X-Api-Key: $API_KEY" https://dns.api.gandi.net/api/v5/domains/$DOMAIN | jq -r '.zone_records_href')
+
+# Update the A Record of the subdomain using PUT
+curl -D- -X PUT -H "Content-Type: application/json" \
+        -H "X-Api-Key: $API_KEY" \
+        -d "{\"rrset_name\": \"$SUBDOMAIN\",
+             \"rrset_type\": \"A\",
+             \"rrset_ttl\": 1200,
+             \"rrset_values\": [\"$EXT_IP\"]}" \
+        $CURRENT_ZONE_HREF/$SUBDOMAIN/A
