76fc186685bdcf1a11d970c6c7d578fbb5380b85
Paho Lurie-Gregg <paho@paholg.com>
2021-08-07 12:53:36 -0700

    starship: tweak settings

--- home/starship.nix
+++ home/starship.nix
@@ -5,12 +5,14 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      hostname.ssh_only = true;
+      git_branch.format = "[$symbol$branch]($style) ";
+      hostname.format = "[$hostname]($style):";
       status.disabled = false;
       time = {
         disabled = false;
         format = "$time ";
       };
+      username.format = "[$user]($style)@";
     };
   };
 }
