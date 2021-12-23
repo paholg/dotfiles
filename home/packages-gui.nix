a49cb855e9c0ce4904073b9901efaff2417165c4
Paho Lurie-Gregg <paho@paholg.com>
2021-12-23 00:18:12 -0800

    home: fix zoom version due to bug

--- home/packages-gui.nix
+++ home/packages-gui.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      zoom-us = super.zoom-us.overrideAttrs (old: rec {
+        version = "5.7.29123.0808";
+
+        src = self.fetchurl {
+          url = "https://zoom.us/client/${version}/zoom_x86_64.pkg.tar.xz";
+          sha256 = "WAeE/2hUaQbWwDg/iqlKSZVoH3ruVHvh+9SEWdPwCIc=";
+        };
+      });
+    })
+  ];
+
   home.packages = with pkgs; [
     adwaita-qt
     arandr
