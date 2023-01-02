f2e4d0c1207176925b21df8f4102e3bc4f982e52
Paho Lurie-Gregg <paho@paholg.com>
2023-01-02 00:06:04 -0800

    box: add IMDB_RENAME_TEMPLATE, remove tvnamer

--- box/home.nix
+++ box/home.nix
@@ -10,6 +10,8 @@
     sessionVariables = {
       IMDB_RENAME_DATA_DIR = "/mnt/storage/imdb-rename";
       IMDB_RENAME_DEST_DIR = "/mnt/storage/";
+      IMDB_RENAME_TEMPLATE =
+        "{{ if episode }} tv/{series.primaryTitle} ({title.startYear})/Season {episode.seasonNumber | leading_zero}/{series.primaryTitle} - s{episode.seasonNumber | leading_zero}e{episode.episodeNumber | leading_zero} - {title.primaryTitle} {{ else }} {title.primaryTitle} ({title.startYear})";
     };
   };
 
@@ -18,6 +20,4 @@
   };
 
   programs.git.userEmail = "paho@paholg.com";
-
-  home.packages = with pkgs; [ tvnamer ];
 }
