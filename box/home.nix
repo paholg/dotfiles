989c968e60c33cce86458a25d843396f20d65a2f
Paho Lurie-Gregg <paho@paholg.com>
2023-01-02 00:11:12 -0800

    box: fix imdb_rename template

--- box/home.nix
+++ box/home.nix
@@ -11,7 +11,7 @@
       IMDB_RENAME_DATA_DIR = "/mnt/storage/imdb-rename";
       IMDB_RENAME_DEST_DIR = "/mnt/storage/";
       IMDB_RENAME_TEMPLATE =
-        "{{ if episode }} tv/{series.primaryTitle} ({title.startYear})/Season {episode.seasonNumber | leading_zero}/{series.primaryTitle} - s{episode.seasonNumber | leading_zero}e{episode.episodeNumber | leading_zero} - {title.primaryTitle} {{ else }} {title.primaryTitle} ({title.startYear})";
+        "{{ if episode }} tv/{series.primaryTitle} ({title.startYear})/Season {episode.seasonNumber | leading_zero}/{series.primaryTitle} - s{episode.seasonNumber | leading_zero}e{episode.episodeNumber | leading_zero} - {title.primaryTitle} {{ else }} movies/{title.primaryTitle} ({title.startYear})";
     };
   };
 
