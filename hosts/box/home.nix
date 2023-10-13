a2bb5aef6e6e6c92abb4507ebb7bff7deed07934
Paho Lurie-Gregg <paho@paholg.com>
2023-10-13 04:59:49 -0700

    remove imdb-rename usage

--- hosts/box/home.nix
+++ hosts/box/home.nix
@@ -1,15 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   imports = [../../home/common.nix ../../home/common-linux.nix];
 
   home = {
     username = "paho";
     homeDirectory = "/home/paho";
 
-    sessionVariables = {
-      IMDB_RENAME_DATA_DIR = "/mnt/storage/imdb-rename";
-      IMDB_RENAME_DEST_DIR = "/mnt/storage/";
-      IMDB_RENAME_TEMPLATE = "{{ if episode -}} tv/{series.primaryTitle}/Season {episode.seasonNumber | leading_zero}/{series.primaryTitle} - s{episode.seasonNumber | leading_zero}e{episode.episodeNumber | leading_zero} - {title.primaryTitle} {{- else -}} movies/{title.primaryTitle} ({title.startYear}) {{- endif }}";
-    };
+    packages = with pkgs; [transmission];
   };
 
   programs.git.userEmail = "paho@paholg.com";
