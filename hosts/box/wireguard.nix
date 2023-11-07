edff852d1f4a005c6d87fe89036ab763dee28e48
Paho Lurie-Gregg <paho@paholg.com>
2023-11-07 01:52:35 -0800

    box: fix wireguard

--- hosts/box/wireguard.nix
+++ hosts/box/wireguard.nix
@@ -1,42 +1,52 @@
-{pkgs, ...}: let
+{...}: let
   port = 51820;
 in {
-  networking = {
-    nat = {
-      enable = true;
-      externalInterface = "eth0";
-      internalInterfaces = ["wg0"];
+  networking.firewall.allowedUDPPorts = [port];
+  systemd.network = {
+    enable = true;
+    netdevs = {
+      "50-wg0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "wg0";
+          MTUBytes = "1300";
+        };
+        wireguardConfig = {
+          PrivateKeyFile = "/wireguard.key";
+          ListenPort = port;
+        };
+        wireguardPeers = [
+          # {
+          #   # Ang - 10.100.0.2
+          #   wireguardPeerConfig = {
+          #     PublicKey = "";
+          #     AllowedIPs = ["0.0.0.0/0" "::/0"];
+          #   };
+          # }
+          {
+            # Pixel 5 - 10.100.0.3
+            wireguardPeerConfig = {
+              PublicKey = "M19tsWObxoO35sBuMTUme71+RhoHx0MKphj0UDnArX8=";
+              AllowedIPs = ["0.0.0.0/0" "::/0"];
+            };
+          }
+          {
+            # Ubuntu - 10.100.0.4
+            wireguardPeerConfig = {
+              PublicKey = "fGW4mrhH1lb63KiITNSpkJMZy6NypbRjDYN/X9xn9gE=";
+              AllowedIPs = ["0.0.0.0/0" "::/0"];
+            };
+          }
+        ];
+      };
     };
-    firewall.allowedUDPPorts = [port];
-    wireguard.interfaces.wg0 = {
-      ips = ["10.100.0.1/24"];
-      listenPort = port;
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eno1 -j MASQUERADE
-      '';
-
-      # This undoes the above command
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eno1 -j MASQUERADE
-      '';
-
-      privateKeyFile = /home/paho/secrets/wireguard.key;
-
-      peers = [
-        {
-          # Ang
-          publicKey = "";
-          allowedIPs = ["0.0.0.0/0" "::/0"];
-        }
-        {
-          # ubuntu
-          publicKey = "fGW4mrhH1lb63KiITNSpkJMZy6NypbRjDYN/X9xn9gE=";
-          allowedIPs = ["0.0.0.0/0" "::/0"];
-        }
-      ];
+    networks.wg0 = {
+      matchConfig.Name = "wg0";
+      address = ["10.100.0.1/24"];
+      networkConfig = {
+        IPMasquerade = "ipv4";
+        IPForward = true;
+      };
     };
   };
 }
