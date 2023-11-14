645a2bd42e624293c9cea5f02b928985632b7967
Paho Lurie-Gregg <paho@paholg.com>
2023-11-13 22:46:00 -0800

    nix: add mosh ports

--- nix/ssh.nix
+++ nix/ssh.nix
@@ -7,6 +7,14 @@
     };
   };
 
+  networking.firewall.allowedUDPPortRanges = [
+    # Ports for Mosh
+    {
+      from = 60001;
+      to = 60999;
+    }
+  ];
+
   users.users.paho.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAz7dvsIeGWRD3zTaenldrKwPJ0z+9fGuDOHkOa4luJd JuiceSSH"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIh01cLJMxFWbyny+uH/nM2j0Mkl3Gar95/6/08fb+J+aFlYnT2Wu6zthZyQ00kblmszIwEgtOOJEfyJOCaPLrs= paho@ubuntu"
