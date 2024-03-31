584548ebe55185da7c81a96f5d4706808b497231
Paho Lurie-Gregg <paho@paholg.com>
2024-03-30 17:49:50 -0700

    nix: add box ssh key

--- nix/ssh.nix
+++ nix/ssh.nix
@@ -22,5 +22,7 @@
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIh01cLJMxFWbyny+uH/nM2j0Mkl3Gar95/6/08fb+J+aFlYnT2Wu6zthZyQ00kblmszIwEgtOOJEfyJOCaPLrs= paho@ubuntu"
     # fractal
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiwxr54qPq+/0gJNA0QGJRWh1VVhTGraYirDQ30wcDI paho@nixos"
+    # box
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINuKvOi7aZTRK6fSxYWJ59t+ep2SUqumNn+J4p5wBzUH paho@box"
   ];
 }
