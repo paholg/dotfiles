af568abe671884e373c10705757933ad4b6b9e88
Paho Lurie-Gregg <paho@paholg.com>
2024-04-01 09:18:52 -0700

    box: use home-manager as nixos module

--- flake.nix
+++ flake.nix
@@ -58,7 +58,6 @@
         display-switch = display-switch.defaultPackage.${prev.system};
         helix = helix.packages.${prev.system}.default;
         ra-multiplex = ra-multiplex.defaultPackage.${prev.system};
-        snippets-ls = snippets-ls.packages.${prev.system}.snippets-ls;
         rustybar = rustybar.defaultPackage.${prev.system};
       };
 
@@ -69,6 +68,34 @@
           # FIXME
           config.allowUnfree = true;
         };
+
+      # use nix flake inputs for registry, for e.g. search.
+      registry = {lib, ...}: {
+        nix.registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
+      };
+      nixos = hosts:
+        builtins.listToAttrs (map (
+            host: {
+              name = host;
+              value = nixpkgs.lib.nixosSystem {
+                pkgs = pkgs linux;
+                system = linux;
+                modules = [
+                  ./hosts/${host}/configuration.nix
+                  home-manager.nixosModules.home-manager
+                  # For `command-not-found`:
+                  inputs.flake-programs-sqlite.nixosModules.programs-sqlite
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.paho = import ./hosts/${host}/home.nix;
+                  }
+                  registry
+                ];
+              };
+            }
+          )
+          hosts);
     in {
       homeConfigurations = {
         "paho@ubuntu" = home-manager.lib.homeManagerConfiguration {
@@ -76,62 +103,11 @@
           modules = [
             ./hosts/ubuntu/home.nix
             nur.nixosModules.nur
-            ({lib, ...}: {
-              nix.registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
-            })
-          ];
-        };
-
-        "paho@box" = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs linux;
-          modules = [
-            ./hosts/box/home.nix
-          ];
-        };
-
-        # "paho@fractal" = home-manager.lib.homeManagerConfiguration {
-        #   pkgs = pkgs linux;
-        #   modules = [
-        #     ./hosts/fractal/home.nix
-        #     nur.nixosModules.nur
-        #   ];
-        # };
-      };
-
-      nixosConfigurations = {
-        box = nixpkgs.lib.nixosSystem {
-          pkgs = pkgs linux;
-          system = linux;
-          modules = [
-            ./hosts/box/configuration.nix
-            # For `command-not-found`:
-            inputs.flake-programs-sqlite.nixosModules.programs-sqlite
-            ({lib, ...}: {
-              nix.registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
-            })
-          ];
-        };
-
-        fractal = nixpkgs.lib.nixosSystem {
-          pkgs = pkgs linux;
-          system = linux;
-          modules = [
-            ./hosts/fractal/configuration.nix
-            home-manager.nixosModules.home-manager
-            # nur.nixosModules.nur
-
-            # For `command-not-found`:
-            inputs.flake-programs-sqlite.nixosModules.programs-sqlite
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.paho = import ./hosts/fractal/home.nix;
-            }
-            ({lib, ...}: {
-              nix.registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
-            })
+            registry
           ];
         };
       };
+
+      nixosConfigurations = nixos ["box" "fractal"];
     };
 }
