f43af0ed358002d02d67247fe96853c6327781af
Paho Lurie-Gregg <paho@paholg.com>
2024-08-16 12:18:58 -0700

    frame: update screen layout

--- hosts/frame/screenlayout/shift.sh
+++ hosts/frame/screenlayout/shift.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-xrandr --output eDP-1 --off \
+xrandr --output eDP-1 --mode 2256x1504 --pos 0x0 --rotate normal \
        --output DP-1 --off \
-       --output DP-2 --primary --mode 3840x2160 --pos 0x0 \
-       --output DP-3 --off \
+       --output DP-2 --off \
+       --output DP-3 --primary --mode 3840x2160 --pos 2256x0 \
        --output DP-4 --off \
        --output DP-5 --off \
        --output DP-6 --off \
