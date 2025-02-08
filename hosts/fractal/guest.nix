10696f1d3aacdbe61e313a5128ee1402ed489f7a
Paho Lurie-Gregg <paho@paholg.com>
2025-02-08 14:23:44 -0800

    fractal: reconfigure guest gamescope

--- hosts/fractal/guest.nix
+++ hosts/fractal/guest.nix
@@ -5,38 +5,60 @@
   ...
 }:
 let
-  gamescopeSession = pkgs.writeShellApplication {
-    name = "gamescope-session";
-    runtimeInputs = with pkgs; [
-      steam
-      gamescope
-    ];
-    text = # bash
-      ''
-        gamescopeArgs=(
-            --adaptive-sync # VRR support
-            --hdr-enabled
-            --rt
-            --steam
-            # See /sys/class/drm/card* for output names that gamescope uses.
-            # They may be different than xrandr.
-            --prefer-output HDMI-A-1
-        )
-        steamArgs=(
-            -pipewire-dmabuf
-            -tenfoot
-        )
+  gamescopeArgsCommon = [
+    "--adaptive-sync" # VRR support
+    "--rt"
+    "--steam"
+  ];
+  gamescopeArgsOffice = lib.strings.concatStringsSep " " (
+    gamescopeArgsCommon
+    ++ [
+      # See /sys/class/drm/card* for output names that gamescope uses.
+      # They may be different than xrandr.
+      "--prefer-output DP-1"
+    ]
+  );
+  gamescopeArgsTv = lib.strings.concatStringsSep " " (
+    gamescopeArgsCommon
+    ++ [
+      "--hdr-enabled"
+      # See /sys/class/drm/card* for output names that gamescope uses.
+      # They may be different than xrandr.
+      "--prefer-output HDMI-A-1"
+    ]
+  );
 
-        exec gamescope "''${gamescopeArgs[@]}" -- steam "''${steamArgs[@]}"
-      '';
+  steamArgs = lib.strings.concatStringsSep " " [
+    "-pipewire-dmabuf"
+    "-tenfoot"
+  ];
+  gamescopeOffice = pkgs.writeShellApplication {
+    name = "gamescope-office";
+    runtimeInputs = [
+      pkgs.gamescope
+      pkgs.steam
+    ];
+    text = "exec gamescope ${gamescopeArgsOffice} -- steam ${steamArgs}";
+  };
+  gamescopeTv = pkgs.writeShellApplication {
+    name = "gamescope-tv";
+    runtimeInputs = [
+      pkgs.gamescope
+      pkgs.steam
+    ];
+    text = "exec gamescope ${gamescopeArgsTv} -- steam ${steamArgs}";
   };
 in
 {
   imports = [
     ../../home
-    ./display_switch.nix
   ];
   home.stateVersion = "24.05";
+
+  home.packages = [
+    gamescopeOffice
+    gamescopeTv
+  ];
 
   custom = {
     username = "guest";
@@ -45,13 +67,12 @@ in
     mangohud.enable = false;
     nixos = true;
     starship.host_color = "cyan";
-    # xfce.enable = true;
     i3.enable = true;
     fish_extra_init = # fish
       ''
         set TTY (tty)
         # TTY1: startx
-        [ "$TTY" = "/dev/tty1" ] && ${lib.getExe gamescopeSession} 2>&1 | tee gamescope.log
+        [ "$TTY" = "/dev/tty1" ] && ${lib.getExe gamescopeTv} 2>&1 | tee gamescope.log
       '';
   };
 
