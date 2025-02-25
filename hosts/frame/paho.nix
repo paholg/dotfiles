f375064ba07add0ffbda740f6085811f072ec204
Paho Lurie-Gregg <paho@paholg.com>
2025-02-25 10:55:23 -0800

    frame: Add more vanta stuff

--- hosts/frame/paho.nix
+++ hosts/frame/paho.nix
@@ -31,7 +31,6 @@
     enable = true;
     plugins = [ pkgs.obs-studio-plugins.obs-backgroundremoval ];
   };
-
   home.packages = with pkgs; [
     awscli2
     csvtool
@@ -42,4 +41,27 @@
     mycli
     pscale
   ];
+
+  # Vanta stuff
+  home.shellAliases.vanta_create = # bash
+    ''
+      distrobox create -ri debian:stable \
+        -n vanta \
+        --home "$HOME/vanta/" \
+        --init \
+        --additional-packages "systemd libpam-systemd pipewire-audio-client-libraries" \
+        --additional-flags "--label keep-true"
+    '';
+  home.shellAliases.vanta_enter = "distrobox enter --root -nw vanta";
+
+  home.file."vanta/check.sh" = {
+    executable = true;
+    text = "/var/vanta/vanta-cli status";
+  };
+
+  home.file."vanta/doctor.sh" = {
+    executable = true;
+    text = "sudo /var/vanta/vanta-cli doctor";
+  };
+
 }
