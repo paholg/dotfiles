e857903a76622a8883c1371d128f1632767d8a72
Paho Lurie-Gregg <paho@paholg.com>
2025-04-23 00:34:29 -0700

    starship: disable jj

--- home/starship.nix
+++ home/starship.nix
@@ -26,58 +26,58 @@
             us-west-2 = "usw2";
           };
         };
-        custom = {
-          jj = {
-            when = true;
-            description = "current jj status";
-            symbol = "";
-            command = # bash
-              ''
-                jj root > /dev/null && jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
-                  separate(" ",
-                    "🥋",
-                    change_id.shortest(4),
-                    bookmarks,
-                    "|",
-                    concat(
-                      if(conflict, "💥"),
-                      if(divergent, "🚧"),
-                      if(hidden, "👻"),
-                      if(immutable, "🔒"),
-                    ),
-                    raw_escape_sequence("\x1b[0;32m") ++ if(empty, "(empty)"),
-                    raw_escape_sequence("\x1b[0;32m") ++ if(description.first_line().len() == 0,
-                      "(no desc)",
-                      if(description.first_line().substr(0, 20) == description.first_line(),
-                        description.first_line(),
-                        description.first_line().substr(0, 20) ++ "…",
-                      )
-                    ) ++ raw_escape_sequence("\x1b[0m"),
-                  )
-                '
-              '';
-          };
-          git_branch = {
-            when = true;
-            command = "jj root > /dev/null 2>&1 || starship module git_branch";
-            description = "Re-enable git when not in a jj repo";
-          };
-          git_commit = {
-            when = true;
-            command = "jj root > /dev/null 2>&1 || starship module git_commit";
-            description = "Re-enable git when not in a jj repo";
-          };
-          git_state = {
-            when = true;
-            command = "jj root > /dev/null 2>&1 || starship module git_state";
-            description = "Re-enable git when not in a jj repo";
-          };
-          git_status = {
-            when = true;
-            command = "jj root > /dev/null 2>&1 || starship module git_status";
-            description = "Re-enable git when not in a jj repo";
-          };
-        };
+        # custom = {
+        #   jj = {
+        #     when = true;
+        #     description = "current jj status";
+        #     symbol = "";
+        #     command = # bash
+        #       ''
+        #         jj root > /dev/null && jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+        #           separate(" ",
+        #             "🥋",
+        #             change_id.shortest(4),
+        #             bookmarks,
+        #             "|",
+        #             concat(
+        #               if(conflict, "💥"),
+        #               if(divergent, "🚧"),
+        #               if(hidden, "👻"),
+        #               if(immutable, "🔒"),
+        #             ),
+        #             raw_escape_sequence("\x1b[0;32m") ++ if(empty, "(empty)"),
+        #             raw_escape_sequence("\x1b[0;32m") ++ if(description.first_line().len() == 0,
+        #               "(no desc)",
+        #               if(description.first_line().substr(0, 20) == description.first_line(),
+        #                 description.first_line(),
+        #                 description.first_line().substr(0, 20) ++ "…",
+        #               )
+        #             ) ++ raw_escape_sequence("\x1b[0m"),
+        #           )
+        #         '
+        #       '';
+        #   };
+        #   git_branch = {
+        #     when = true;
+        #     command = "jj root > /dev/null 2>&1 || starship module git_branch";
+        #     description = "Re-enable git when not in a jj repo";
+        #   };
+        #   git_commit = {
+        #     when = true;
+        #     command = "jj root > /dev/null 2>&1 || starship module git_commit";
+        #     description = "Re-enable git when not in a jj repo";
+        #   };
+        #   git_state = {
+        #     when = true;
+        #     command = "jj root > /dev/null 2>&1 || starship module git_state";
+        #     description = "Re-enable git when not in a jj repo";
+        #   };
+        #   git_status = {
+        #     when = true;
+        #     command = "jj root > /dev/null 2>&1 || starship module git_status";
+        #     description = "Re-enable git when not in a jj repo";
+        #   };
+        # };
         hostname = {
           format = "[$hostname]($style):";
           style = "bold ${config.custom.starship.host_color}";
