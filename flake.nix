753c9f915a061328f0886302bcd455fa9f8f2595
Paho Lurie-Gregg <paho@paholg.com>
2025-07-11 21:09:12 -0700

    flake: fix dependency

--- flake.nix
+++ flake.nix
@@ -47,7 +47,6 @@
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
       inputs.systems.follows = "systems";
     };
 
