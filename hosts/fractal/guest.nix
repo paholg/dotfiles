46a8c49ae28c63cd839341e807f382d8c7b46f0d
Paho Lurie-Gregg <paho@paholg.com>
2025-10-18 16:53:26 -0700

    fractal: guest back to gamescope

--- hosts/fractal/guest.nix
+++ hosts/fractal/guest.nix
@@ -1,9 +1,63 @@
-{ lib, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  gamescopeArgsCommon = [
+    "--adaptive-sync" # VRR support
+    "--rt"
+    "--steam"
+  ];
+  gamescopeArgsOffice = lib.strings.concatStringsSep " " (
+    gamescopeArgsCommon
+    ++ [
+      # See /sys/class/drm/card* for output names that gamescope uses.
+      # They may be different than xrandr.
+      "--prefer-output DP-1"
+    ]
+  );
+  gamescopeArgsTv = lib.strings.concatStringsSep " " (
+    gamescopeArgsCommon
+    ++ [
+      "--hdr-enabled"
+      # See /sys/class/drm/card* for output names that gamescope uses.
+      # They may be different than xrandr.
+      "--prefer-output DP-2"
+    ]
+  );
+
+  steamArgs = lib.strings.concatStringsSep " " [
+    "-pipewire-dmabuf"
+    "-tenfoot"
+  ];
+  gamescopeOffice = pkgs.writeShellApplication {
+    name = "gamescope-office";
+    runtimeInputs = [
+      pkgs.gamescope
+      pkgs.steam
+    ];
+    text = "exec gamescope ${gamescopeArgsOffice} -- steam ${steamArgs}";
+  };
+  gamescopeTv = pkgs.writeShellApplication {
+    name = "gamescope-tv";
+    runtimeInputs = [
+      pkgs.gamescope
+      pkgs.steam
+    ];
+    text = "exec gamescope ${gamescopeArgsTv} -- steam ${steamArgs}";
+  };
+in
 {
   imports = [
     ../../home
   ];
   home.stateVersion = "24.05";
+
+  home.packages = [
+    gamescopeOffice
+    gamescopeTv
+  ];
 
   custom = {
     username = "guest";
@@ -14,7 +68,7 @@
       ''
         set TTY (tty)
         # TTY1: startx
-        [ "$TTY" = "/dev/tty1" ] && exec "niri-session"
+        [ "$TTY" = "/dev/tty1" ] && ${lib.getExe gamescopeTv} 2>&1 | tee gamescope.log
       '';
   };
 
