73f5b7c56688a65353226c73876444e4f6cf6be0
Paho Lurie-Gregg <paho@paholg.com>
2025-11-30 00:08:28 -0800

    box: Enable kanidm
    We now have kanidm working behind nginx with oauth2-proxy!

--- hosts/box/media.nix
+++ hosts/box/media.nix
@@ -4,6 +4,27 @@
   pkgs,
   ...
 }:
+let
+  authExtraConfig = ''
+    auth_request /oauth2/auth;
+    error_page 401 = /oauth2/sign_in;
+
+    auth_request_set $user $upstream_http_x_auth_request_user;
+    auth_request_set $email $upstream_http_x_auth_request_email;
+    proxy_set_header X-User $user;
+    proxy_set_header X-Email $email;
+
+    auth_request_set $auth_cookie $upstream_http_set_cookie;
+    add_header Set-Cookie $auth_cookie;
+  '';
+
+  mkAuthLocation = location: proxyPass: {
+    ${location} = {
+      inherit proxyPass;
+      extraConfig = authExtraConfig;
+    };
+  };
+in
 {
   age.secrets = {
     porkbun_api = {
@@ -11,6 +32,9 @@
     };
     vpn_config = {
       file = ../../secrets/vpn_config;
+    };
+    oauth2_proxy = {
+      file = ../../secrets/oauth2_proxy;
     };
   };
 
@@ -49,81 +73,39 @@
         locations."/sonarr".proxyPass = "http://localhost:${toString config.custom.ports.sonarr}/sonarr";
       };
 
-      virtualHosts."auth.paholg.com" = {
-        enableACME = true;
-        forceSSL = true;
-      };
-
-      virtualHosts."plex.paholg.com" = {
-        enableACME = true;
-        forceSSL = true;
-        http2 = true;
-        extraConfig = ''
-
-          #Some players don't reopen a socket and playback stops totally instead of resuming after an extended pause
-          send_timeout 100m;
-
-          # Why this is important: https://blog.cloudflare.com/ocsp-stapling-how-cloudflare-just-made-ssl-30/
-          ssl_stapling on;
-          ssl_stapling_verify on;
-
-          ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-          ssl_prefer_server_ciphers on;
-          #Intentionally not hardened for security for player support and encryption video streams has a lot of overhead with something like AES-256-GCM-SHA384.
-          ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
-
-          # Forward real ip and host to Plex
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header Host $server_addr;
-          proxy_set_header Referer $server_addr;
-          proxy_set_header Origin $server_addr; 
-
-          # Plex has A LOT of javascript, xml and html. This helps a lot, but if it causes playback issues with devices turn it off.
-          gzip on;
-          gzip_vary on;
-          gzip_min_length 1000;
-          gzip_proxied any;
-          gzip_types text/plain text/css text/xml application/xml text/javascript application/x-javascript image/svg+xml;
-          gzip_disable "MSIE [1-6]\.";
-
-          # Nginx default client_max_body_size is 1MB, which breaks Camera Upload feature from the phones.
-          # Increasing the limit fixes the issue. Anyhow, if 4K videos are expected to be uploaded, the size might need to be increased even more
-          client_max_body_size 100M;
-
-          # Plex headers
-          proxy_set_header X-Plex-Client-Identifier $http_x_plex_client_identifier;
-          proxy_set_header X-Plex-Device $http_x_plex_device;
-          proxy_set_header X-Plex-Device-Name $http_x_plex_device_name;
-          proxy_set_header X-Plex-Platform $http_x_plex_platform;
-          proxy_set_header X-Plex-Platform-Version $http_x_plex_platform_version;
-          proxy_set_header X-Plex-Product $http_x_plex_product;
-          proxy_set_header X-Plex-Token $http_x_plex_token;
-          proxy_set_header X-Plex-Version $http_x_plex_version;
-          proxy_set_header X-Plex-Nocache $http_x_plex_nocache;
-          proxy_set_header X-Plex-Provides $http_x_plex_provides;
-          proxy_set_header X-Plex-Device-Vendor $http_x_plex_device_vendor;
-          proxy_set_header X-Plex-Model $http_x_plex_model;
-
-          # Websockets
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "upgrade";
-
-          # Buffering off send to the client as soon as the data is received from Plex.
-          proxy_redirect off;
-          proxy_buffering off;
-        '';
-        locations."/" = {
-          proxyPass = "http://localhost:32400/";
-        };
-      };
-
       virtualHosts."tv.paholg.com" = {
         enableACME = true;
         forceSSL = true;
         locations."/".proxyPass = "http://localhost:${toString config.custom.ports.jellyfin}";
+      };
+
+      virtualHosts."home.paholg.com" = {
+        enableACME = true;
+        forceSSL = true;
+        root = pkgs.runCommand "home-paholg-com-root" { } ''
+          mkdir -p $out
+          cp ${./home-index.html} $out/index.html
+        '';
+
+        locations = lib.mkMerge [
+          {
+            "/oauth2/" = {
+              proxyPass = "http://127.0.0.1:${toString config.custom.ports.oauth2_proxy}";
+              extraConfig = ''
+                proxy_set_header X-Scheme $scheme;
+                proxy_set_header X-Auth-Request-Redirect $request_uri;
+              '';
+            };
+            "/" = {
+              index = "index.html";
+              extraConfig = authExtraConfig;
+            };
+          }
+          (mkAuthLocation "/prowlarr" "http://localhost:${toString config.custom.ports.prowlarr}/prowlarr")
+          (mkAuthLocation "/radarr" "http://localhost:${toString config.custom.ports.radarr}/radarr")
+          (mkAuthLocation "/sonarr" "http://localhost:${toString config.custom.ports.sonarr}/sonarr")
+          (mkAuthLocation "/transmission" "http://${config.custom.ips.container}:${toString config.custom.ports.transmission}")
+        ];
       };
     };
 
@@ -162,6 +144,31 @@
       group = "media";
       openFirewall = false;
       dataDir = config.custom.drives.storage + "/sonarr";
+    };
+
+    oauth2-proxy = {
+      enable = true;
+      provider = "oidc";
+      httpAddress = "http://127.0.0.1:${toString config.custom.ports.oauth2_proxy}";
+      reverseProxy = true;
+
+      redirectURL = "https://home.paholg.com/oauth2/callback";
+      oidcIssuerUrl = "https://auth.paholg.com/oauth2/openid/oauth2-proxy";
+
+      keyFile = config.age.secrets.oauth2_proxy.path;
+
+      email.domains = [ "*" ];
+
+      setXauthrequest = true;
+
+      cookie = {
+        domain = ".paholg.com";
+        secure = true;
+      };
+
+      extraConfig = {
+        code-challenge-method = "S256";
+      };
     };
   };
 
