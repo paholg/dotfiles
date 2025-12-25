31a72ff5aa21623d19af9787081f41432d3ceddb
Paho Lurie-Gregg <paho@paholg.com>
2025-12-24 23:38:26 -0800

    box: configure foundry vtt

--- hosts/box/foundry-vtt.nix
+++ hosts/box/foundry-vtt.nix
@@ -17,9 +17,20 @@
   virtualisation.oci-containers.containers.foundry = {
     user = "${toString config.custom.uids.foundry}:${toString config.custom.groups.foundry}";
     image = "felddy/foundryvtt:13.351";
+    hostname = "vtt.paholg.com";
     ports = [ "0.0.0.0:${toString config.custom.ports.foundry}:30000" ];
     environmentFiles = [ config.age.secrets.foundry_env.path ];
-    volumes = [ "${config.custom.drives.storage}/foundry:/data" ];
+    environment = {
+      FOUNDRY_HOSTNAME = "vtt.paholg.com";
+      FOUNDRY_PROXY_SSL = "true";
+      FOUNDRY_PROXY_PORT = "443";
+    };
+    volumes = [
+      "${config.custom.drives.storage}/foundry:/data"
+      # Keep the install until the cache is fixed:
+      # https://github.com/felddy/foundryvtt-docker/issues/1333
+      "${config.custom.drives.storage}/foundry/resources:/home/node/resources"
+    ];
   };
 
   services.nginx.virtualHosts."vtt.paholg.com" = {
@@ -28,6 +39,13 @@
     locations."/" = {
       proxyPass = "http://localhost:${toString config.custom.ports.foundry}";
       proxyWebsockets = true;
+      extraConfig = ''
+        # Disable proxy buffering for faster streaming
+        proxy_buffering off;
+
+        # Increase max body size for asset uploads
+        client_max_body_size 300M;
+      '';
     };
   };
 }
