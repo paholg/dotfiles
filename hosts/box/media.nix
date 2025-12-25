759116a51903c6aa329f78529f0952ac4e410933
Paho Lurie-Gregg <paho@paholg.com>
2025-12-25 11:48:11 -0800

    box: fine-grained home.paholg.com permissions
    We configure kanidm to let any user use oauth2-proxy, and configure
    what is showed/allowed based on their groups.

--- hosts/box/media.nix
+++ hosts/box/media.nix
@@ -5,12 +5,16 @@
   ...
 }:
 let
-  authExtraConfig = ''
-    auth_request /oauth2/auth;
+  oauth2Port = toString config.custom.ports.oauth2_proxy;
+
+  authExtraConfig = internalAuthLocation: ''
+    auth_request ${internalAuthLocation};
     error_page 401 = /oauth2/sign_in;
 
     auth_request_set $user $upstream_http_x_auth_request_user;
+    auth_request_set $preferred_username $upstream_http_x_auth_request_preferred_username;
     auth_request_set $email $upstream_http_x_auth_request_email;
+    auth_request_set $groups $upstream_http_x_auth_request_groups;
     proxy_set_header X-User $user;
     proxy_set_header X-Email $email;
 
@@ -18,12 +22,29 @@ let
     add_header Set-Cookie $auth_cookie;
   '';
 
-  mkAuthLocation = location: proxyPass: {
-    ${location} = {
-      inherit proxyPass;
-      extraConfig = authExtraConfig;
+  mkAuthLocation =
+    location: proxyPass: groups:
+    let
+      groupsParam = if groups == [ ] then "" else "?allowed_groups=${lib.concatStringsSep "," groups}";
+      internalAuthLocation = "/internal/auth${location}";
+    in
+    {
+      ${internalAuthLocation} = {
+        extraConfig = ''
+          internal;
+          proxy_pass http://127.0.0.1:${oauth2Port}/oauth2/auth${groupsParam};
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Scheme $scheme;
+          proxy_set_header Content-Length "";
+          proxy_pass_request_body off;
+        '';
+      };
+      ${location} = {
+        inherit proxyPass;
+        extraConfig = authExtraConfig internalAuthLocation;
+      };
     };
-  };
 in
 {
   age.secrets = {
@@ -80,21 +101,44 @@ in
         locations = lib.mkMerge [
           {
             "/oauth2/" = {
-              proxyPass = "http://127.0.0.1:${toString config.custom.ports.oauth2_proxy}";
+              proxyPass = "http://127.0.0.1:${oauth2Port}";
               extraConfig = ''
                 proxy_set_header X-Scheme $scheme;
                 proxy_set_header X-Auth-Request-Redirect $request_uri;
               '';
             };
+            "/internal/auth/" = {
+              extraConfig = ''
+                internal;
+                proxy_pass http://127.0.0.1:${oauth2Port}/oauth2/auth;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Scheme $scheme;
+                proxy_set_header Content-Length "";
+                proxy_pass_request_body off;
+              '';
+            };
             "/" = {
               index = "index.html";
-              extraConfig = authExtraConfig;
+              extraConfig = ''
+                ssi on;
+                ${authExtraConfig "/internal/auth/"}
+              '';
             };
           }
-          (mkAuthLocation "/prowlarr" "http://localhost:${toString config.custom.ports.prowlarr}/prowlarr")
-          (mkAuthLocation "/radarr" "http://localhost:${toString config.custom.ports.radarr}/radarr")
-          (mkAuthLocation "/sonarr" "http://localhost:${toString config.custom.ports.sonarr}/sonarr")
-          (mkAuthLocation "/transmission" "http://${config.custom.ips.container}:${toString config.custom.ports.transmission}")
+          (mkAuthLocation "/prowlarr" "http://localhost:${toString config.custom.ports.prowlarr}/prowlarr" [
+            "arr_admin@auth.paholg.com"
+          ])
+          (mkAuthLocation "/radarr" "http://localhost:${toString config.custom.ports.radarr}/radarr" [
+            "arr_admin@auth.paholg.com"
+          ])
+          (mkAuthLocation "/sonarr" "http://localhost:${toString config.custom.ports.sonarr}/sonarr" [
+            "arr_admin@auth.paholg.com"
+          ])
+          (mkAuthLocation "/transmission"
+            "http://${config.custom.ips.container}:${toString config.custom.ports.transmission}"
+            [ "arr_admin@auth.paholg.com" ]
+          )
         ];
       };
     };
@@ -158,6 +202,8 @@ in
 
       extraConfig = {
         code-challenge-method = "S256";
+        scope = "openid email profile groups";
+        whitelist-domain = ".paholg.com";
       };
     };
   };
