8b1241581b2cea034c94dceb847baffb696e28f8
Paho Lurie-Gregg <paho@paholg.com>
2026-03-29 15:19:41 -0700

    gui: Fix audioengine speaker config

--- nixos/gui/default.nix
+++ nixos/gui/default.nix
@@ -35,6 +35,24 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+
+      # The Audioengine HD3 uses a CSR Bluetooth chip (0a12:1243) whose USB
+      # product string resolves to "CSRA64210 [TaoTronics Headset BH-22 ...]"
+      # in the USB ID database. Override so apps show the real name.
+      wireplumber.extraConfig."50-audioengine-hd3" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "device.bus-id" = "usb-Audioengine_Audioengine_HD3_B40020170802-00"; }
+            ];
+            actions.update-props = {
+              "device.description" = "Audioengine HD3";
+              "api.acp.auto-profile" = false;
+              "device.profile" = "output:analog-stereo";
+            };
+          }
+        ];
+      };
     };
 
     hardware.printers.ensurePrinters = [
@@ -45,6 +63,12 @@
         model = "samsung/M267x.ppd";
       }
     ];
+
+    # Disable USB autosuspend on audio devices to prevent broken pipe
+    # loops when the device wakes from suspend mid-stream.
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0a12", ATTR{idProduct}=="1243", ATTR{power/control}="on", ATTR{power/autosuspend_delay_ms}="-1"
+    '';
 
     services = {
       blueman.enable = true;
